a8d6cfde4e923d65178e2c6be2588abfa392e857
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-07-02 22:35:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@
 // import getOptions from './get-options';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const api_key =
+const API_KEY =
   'live_7ctoNlYDqvRmUrjGDIjzkXX0AcLi54Bj3E5E1IL8zd4wg4HCVIG5ZdJbaWtVOXLA';
 
 function fetchBreeds() {
-  const url = `https://api.thecatapi.com/v1/breeds/`;
-
-  return fetch(url, {
-    headers: {
-      'x-api-key': api_key,
-    },
-  }).then(response => {
+  const url = 'https://api.thecatapi.com/v1/breeds';
+  return fetch(url, { headers: { 'x-api-key': API_KEY } }).then(response => {
     return response.json();
   });
 }
